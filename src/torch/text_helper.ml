f6c73a93b02e9ed4fca8e8ecf29cfb32aba79a7a
Laurent <laurent.mazare@gmail.com>
2018-10-27 18:30:31 +0100

    Add some text dataset helper.

---
+++ src/torch/text_helper.ml
@@ -0,0 +1,48 @@
+open Base
+
+type t =
+  { content : Tensor.t
+  ; char_for_label : char Map.M(Int).t
+  }
+
+let create ~filename =
+  let file_descr = Unix.openfile filename [ O_RDONLY ] 0 in
+  let content =
+    Unix.map_file file_descr Int8_unsigned C_layout false [|-1|]
+    |> Bigarray.array1_of_genarray
+  in
+  Unix.close file_descr;
+  let label_for_char = Hashtbl.Poly.create () in
+  for i = 0 to Bigarray.Array1.dim content - 1 do
+    content.{i} <-
+      Hashtbl.find_or_add label_for_char content.{i}
+        ~default:(fun () -> Hashtbl.length label_for_char)
+  done;
+  let char_for_label =
+    Hashtbl.to_alist label_for_char
+    |> List.map ~f:(fun (char, label) -> label, Char.of_int_exn char)
+    |> Map.of_alist_exn (module Int)
+  in
+  { content = Bigarray.genarray_of_array1 content |> Tensor.of_bigarray
+  ; char_for_label
+  }
+
+let total_length t = Tensor.shape t.content |> List.hd_exn
+let char t ~label = Map.find_exn t.char_for_label label
+let labels t = Map.length t.char_for_label
+
+let iter ?device t ~f ~seq_len ~batch_size =
+  let total_length = total_length t in
+  let start_indexes = Tensor.randperm ~n:(total_length - seq_len) ~options:(Int64, Cpu) in
+  for index = 0 to (total_length - seq_len - 1) / batch_size do
+    let xs, ys =
+      List.init batch_size ~f:(fun i ->
+        let start = Tensor.get start_indexes (index * batch_size + i) |> Tensor.int_value in
+        Tensor.narrow t.content ~dim:0 ~start ~length:seq_len,
+        Tensor.narrow t.content ~dim:0 ~start:(start + 1) ~length:seq_len)
+      |> List.unzip
+    in
+    let xs = Tensor.cat xs ~dim:1 |> Tensor.to_device ?device in
+    let ys = Tensor.cat ys ~dim:1 |> Tensor.to_device ?device in
+    f index ~xs ~ys
+  done
---
+++ src/torch/text_helper.mli
@@ -0,0 +1,37 @@
+open! Base
+
+type t
+
+(** [create ~filename] reads text file [filename] and stores its content in
+    [t].
+    The tensors returned in this module use compact labels: e.g. if [filename]
+    use n different characters, the values used in the tensors will range from
+    0 to n-1.
+*)
+val create : filename:string -> t
+
+(** [iter t ~f ~seq_len ~batch_size] iterates [f] over the whole
+    dataset. [f] is given two different tensors [xs] and [ys] which
+    shapes are both [seq_len; batch_size; labels]. [ys] is shifted by one
+    compared to [xs].
+    The dataset is shuffled on each call to [iter].
+*)
+val iter
+  :  ?device:Torch_core.Device.t
+  -> t
+  -> f:(int -> xs:Tensor.t -> ys:Tensor.t -> unit)
+  -> seq_len:int
+  -> batch_size:int
+  -> unit
+
+(** [char t ~label] returns the character from the original file that has been
+    mapped to [label].
+*)
+val char : t -> label:int -> char
+
+val total_length : t -> int
+
+(** [labels t] returns the number of different labels, i.e. the number of
+    distinct chars in the original file.
+*)
+val labels : t -> int
