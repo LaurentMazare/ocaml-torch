ff5dd00568bb1b8e329a4dc04a5b0f9035a0a9ce
Laurent Mazare <laurent.mazare@gmail.com>
2018-11-01 10:41:46 +0000

    Bugfix.

--- src/torch/n.ml
+++ src/torch/n.ml
@@ -3,5 +3,5 @@ open Base
 type t = string list
 
 let of_list = Fn.id
-let to_string t = List.rev t |> String.concat ~sep:"."
+let to_string t = List.rev t |> String.concat ~sep:"|"
 let (/) t p = p :: t
