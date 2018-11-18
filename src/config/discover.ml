bf5b508e6d41ffe1d18bce6b78daca1f1ac65fcf
laurent <laurent.mazare@gmail.com>
2018-11-18 04:44:42 +0000

    Use /usr/local/cuda when available.

--- src/config/discover.ml
+++ src/config/discover.ml
@@ -1,16 +1,14 @@
 open Base
 module C = Configurator.V1
 
+let empty_flags = { C.Pkg_config.cflags = []; libs = [] }
+let combine (flags1 : C.Pkg_config.package_conf) (flags2 : C.Pkg_config.package_conf) =
+  { C.Pkg_config.cflags = flags1.cflags @ flags2.cflags
+  ; libs = flags1.libs @ flags2.libs
+  }
+
 let extract_flags c ~package =
-  match C.Pkg_config.get c with
-  | None -> [], []
-  | Some pc ->
-    let cuda_cxx_flags, cuda_c_library_flags =
-      match C.Pkg_config.query pc ~package with
-      | None -> [], []
-      | Some deps -> deps.cflags, deps.libs
-    in
-    cuda_cxx_flags, cuda_c_library_flags
+  Option.bind (C.Pkg_config.get c) ~f:(C.Pkg_config.query ~package)
 
 let torch_flags () =
   let libroot_include, libroot_lib =
@@ -39,30 +37,51 @@ let torch_flags () =
       | [] -> Printf.failwithf "no python directory with torch found in %s" conda_prefix ()
       | libdir :: _ -> libdir ^ "/include", libdir
   in
-  let cxx_flags =
+  let cflags =
     [ "-isystem" ; Printf.sprintf "%s" libroot_include
     ; "-isystem"; Printf.sprintf "%s/torch/csrc/api/include" libroot_include
     ]
   in
-  let c_library_flags =
+  let libs =
     [ Printf.sprintf "-Wl,-R%s" libroot_lib
     ; Printf.sprintf "-L%s" libroot_lib
     ; "-lc10"; "-lcaffe2"; "-ltorch"
     ]
   in
-  cxx_flags, c_library_flags
+  { C.Pkg_config.cflags; libs }
+
+let libcuda_flags ~lcuda ~lnvrtc =
+  let cudadir = "/usr/local/cuda/lib64" in
+  if Caml.Sys.file_exists cudadir && Caml.Sys.is_directory cudadir
+  then
+    let libs =
+      [ Printf.sprintf "-Wl,-R%s" cudadir
+      ; Printf.sprintf "-L%s" cudadir
+      ]
+    in
+    let libs = if lcuda then libs @ [ "-lcuda" ] else libs in
+    let libs = if lnvrtc then libs @ [ "-lnvrtc" ] else libs in
+    { C.Pkg_config.cflags = [] ; libs }
+  else empty_flags
 
 let () =
   C.main ~name:"torch-config" (fun c ->
-      let cxx_flags, c_library_flags =
+      let torch_flags =
         try
           torch_flags ()
         with
-        | _ -> [], []
+        | _ -> empty_flags
       in
-      let cuda_cxx_flags, cuda_c_library_flags = extract_flags c ~package:"cuda" in
-      let nvrtc_cxx_flags, nvrtc_c_library_flags = extract_flags c ~package:"nvrtc" in
-      C.Flags.write_sexp "cxx_flags.sexp"
-        (cxx_flags @ cuda_cxx_flags @ nvrtc_cxx_flags);
-      C.Flags.write_sexp "c_library_flags.sexp"
-        (c_library_flags @ cuda_c_library_flags @ nvrtc_c_library_flags))
+      let cuda_flags = extract_flags c ~package:"cuda" in
+      let nvrtc_flags = extract_flags c ~package:"nvrtc" in
+      let cuda_flags =
+        match cuda_flags, nvrtc_flags with
+        | None, None -> libcuda_flags ~lcuda:true ~lnvrtc:true
+        | Some cuda_flags, None ->
+          combine cuda_flags (libcuda_flags ~lcuda:false ~lnvrtc:true)
+        | None, Some nvrtc_flags ->
+          combine nvrtc_flags (libcuda_flags ~lcuda:true ~lnvrtc:false)
+        | Some cuda_flags, Some nvrtc_flags -> combine cuda_flags nvrtc_flags
+      in
+      C.Flags.write_sexp "cxx_flags.sexp" (torch_flags.cflags @ cuda_flags.cflags);
+      C.Flags.write_sexp "c_library_flags.sexp" (torch_flags.libs @ cuda_flags.libs))
