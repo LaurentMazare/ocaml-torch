1406c66b981bcf106f24d03aedbb8d7657e16378
laurent <laurent.mazare@gmail.com>
2018-12-10 23:19:27 +0000

    Add some tests for serialization.

---
+++ src/tests/serialization_tests.ml
@@ -0,0 +1,57 @@
+open Base
+open Torch
+
+let write_and_read tensor ~print_tensor =
+  let filename = Caml.Filename.temp_file "torchtest" ".ot" in
+  Serialize.save tensor ~filename;
+  let y = Serialize.load ~filename in
+  let l2 = Tensor.((tensor - y) * (tensor - y)) |> Tensor.sum in
+  print_tensor l2;
+  Unix.unlink filename
+
+let%expect_test _ =
+  let print_tensor tensor = Stdio.printf "%d\n" (Tensor.to_int0_exn tensor) in
+  Tensor.randint1 ~high:42 ~size:[ 3; 1; 4 ] ~options:(Int64, Cpu)
+  |> write_and_read ~print_tensor;
+  [%expect{|
+        0
+      |}];
+  write_and_read (Tensor.of_int0 1337) ~print_tensor;
+  [%expect{|
+        0
+      |}]
+
+let%expect_test _ =
+  let print_tensor tensor = Stdio.printf "%f\n" (Tensor.to_float0_exn tensor) in
+  write_and_read (Tensor.randn [ 42; 27 ]) ~print_tensor;
+  [%expect{|
+        0.000000
+      |}];
+  write_and_read (Tensor.of_float0 1337.) ~print_tensor;
+  [%expect{|
+        0.000000
+      |}]
+
+let write_and_read named_tensors =
+  let filename = Caml.Filename.temp_file "torchtest" ".ot" in
+  Serialize.save_multi ~named_tensors ~filename;
+  let ys = Serialize.load_multi ~names:(List.map named_tensors ~f:fst) ~filename in
+  List.iter2_exn named_tensors ys ~f:(fun (name, tensor) y ->
+    let l2 = Tensor.((tensor - y) * (tensor - y)) |> Tensor.sum in
+    match Tensor.kind l2 with
+    | Int64 | Int -> Stdio.printf "%s %d\n%!" name (Tensor.to_int0_exn l2)
+    | Float | Double -> Stdio.printf "%s %f\n%!" name (Tensor.to_float0_exn l2)
+    | _ -> assert false);
+  Unix.unlink filename
+
+let%expect_test _ =
+  write_and_read
+    [ "tensor-1", Tensor.of_float1 [| 3.; 14.; 15.; 9265.35 |]
+    ; "another", Tensor.of_int0 42
+    ; "and yet another",  Tensor.of_int2 [| [| 3; -1; -51234 |]; [| 2718; 2818; 28 |] |]
+    ];
+  [%expect{|
+        tensor-1 0.000000
+        another 0
+        and yet another 0
+      |}]
