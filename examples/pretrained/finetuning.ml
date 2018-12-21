6f10237a98ac07fa55f4d6c016a008673fe102cf
laurent <laurent.mazare@gmail.com>
2018-12-21 09:05:46 +0000

    Resnet finetuning example tweaks.

--- examples/pretrained/finetuning.ml
+++ examples/pretrained/finetuning.ml
@@ -15,9 +15,10 @@ open Torch
 open Torch_vision
 
 let batch_size = 8
+let classes = [ "ants"; "bees" ]
 
+(* Precompute the last layer of the pre-trained model on the whole dataset. *)
 let precompute_activations dataset ~model_path =
-  (* Precompute the last layer of the pre-trained model on the whole dataset. *)
   let dataset =
     let frozen_vs = Var_store.create ~frozen:true ~name:"rn" () in
     let pretrained_model = Resnet.resnet18 frozen_vs in
@@ -35,36 +36,30 @@ let precompute_activations dataset ~model_path =
 let () =
   if Array.length Sys.argv <> 3
   then Printf.failwithf "usage: %s resnet18.ot dataset-path" Sys.argv.(0) ();
-  let dataset = Imagenet.load_dataset ~dir:Sys.argv.(2) ~classes:[ "ants"; "bees" ] () in
+  let dataset = Imagenet.load_dataset ~dir:Sys.argv.(2) ~classes () in
   Dataset_helper.print_summary dataset;
   let dataset = precompute_activations dataset ~model_path:Sys.argv.(1) in
 
   let train_vs = Var_store.create ~name:"rn-vs" () in
-  let fc1 = Layer.linear train_vs ~input_dim:512 2 in
+  let fc1 = Layer.linear train_vs ~input_dim:512 (List.length classes) in
   let model xs = Layer.apply fc1 xs in
 
   let sgd = Optimizer.sgd train_vs ~learning_rate:0.001 ~momentum:0.9 in
   for epoch_idx = 1 to 20 do
-    let start_time = Unix.gettimeofday () in
     let sum_loss = ref 0. in
-    Dataset_helper.iter dataset ~batch_size ~f:(fun b_idx ~batch_images ~batch_labels ->
+    Dataset_helper.iter dataset ~batch_size ~f:(fun _ ~batch_images ~batch_labels ->
       let predicted = model batch_images in
       (* Compute the cross-entropy loss. *)
       let loss = Tensor.cross_entropy_for_logits predicted ~targets:batch_labels in
       sum_loss := !sum_loss +. Tensor.float_value loss;
-      Stdio.printf "%d/%d %f\r%!"
-        (1 + b_idx)
-        (Dataset_helper.batches_per_epoch dataset ~batch_size)
-        (!sum_loss /. Float.of_int (1 + b_idx));
       Optimizer.backward_step sgd ~loss);
 
     (* Compute the validation error. *)
     let test_accuracy =
       Dataset_helper.batch_accuracy dataset `test ~batch_size ~predict:model
     in
-    Stdio.printf "%d %.0fs %f %.2f%%\n%!"
+    Stdio.printf "%3d   train loss: %f   test accuracy: %.2f%%\n%!"
       epoch_idx
-      (Unix.gettimeofday () -. start_time)
       (!sum_loss /. Float.of_int (Dataset_helper.batches_per_epoch dataset ~batch_size))
       (100. *. test_accuracy);
   done
--- src/vision/resnet.mli
+++ src/vision/resnet.mli
@@ -1,4 +1,10 @@
 open Torch
+
+(* If [num_classes] is provided, the last layer is a fully-connected layer with
+   [num_classes] output. When [num_classes] is not provided the values before
+   this last layer are returned - this is useful to use these models as feature
+   extractors.
+*)
 val resnet18  : ?num_classes:int -> Var_store.t -> Layer.t_with_training
 val resnet34  : ?num_classes:int -> Var_store.t -> Layer.t_with_training
 val resnet50  : ?num_classes:int -> Var_store.t -> Layer.t_with_training
