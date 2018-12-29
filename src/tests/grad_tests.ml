555eefb3a74329703a1186fd56eb19f5c2c00574
laurent <laurent.mazare@gmail.com>
2018-12-29 21:20:04 +0000

    Add some backprop tests.

--- src/tests/grad_tests.ml
+++ src/tests/grad_tests.ml
@@ -5,6 +5,29 @@ open Torch
 let%expect_test _ =
   let x = Tensor.f 42. |> Tensor.set_requires_grad ~r:true in
   let y = Tensor.(x * x * x + x * x) in
+  Tensor.zero_grad x;
+  Tensor.backward y;
+  let dy_over_dx = Tensor.grad x in
+  Stdio.printf !"%{sexp:float}\n" (Tensor.to_float0_exn dy_over_dx);
+  [%expect{|
+        5376
+      |}]
+
+let%expect_test _ =
+  let x = Tensor.f 42. |> Tensor.set_requires_grad ~r:true in
+  let y = Tensor.(x * x * x + x * x) in
+  Tensor.zero_grad x;
+  let dy_over_dx = Tensor.run_backward [ y ] [ x ] ~create_graph:true ~keep_graph:true |> List.hd_exn in
+  Tensor.backward dy_over_dx;
+  let dy_over_dx2 = Tensor.grad x in
+  Stdio.printf !"%{sexp:float}\n" (Tensor.to_float0_exn dy_over_dx2);
+  [%expect{|
+        254
+      |}]
+
+let%expect_test _ =
+  let x = Tensor.f 42. |> Tensor.set_requires_grad ~r:true in
+  let y = Tensor.(x * x * x + x * x) in
   let dy_over_dx = Tensor.run_backward [ y ] [ x ] |> List.hd_exn in
   Stdio.printf !"%{sexp:float}\n" (Tensor.to_float0_exn dy_over_dx);
   [%expect{|
