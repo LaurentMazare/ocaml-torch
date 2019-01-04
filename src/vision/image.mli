b47b98d61815c42d9bd2001eef0095f3a5751eaf
Laurent <laurent.mazare@gmail.com>
2019-01-04 19:28:28 +0000

    Add some documentation.

--- src/vision/image.mli
+++ src/vision/image.mli
@@ -1,11 +1,21 @@
 open Base
 open Torch
 
+(** [load_image ?resize filename] returns a tensor containing the pixels for the image
+    in [filename]. Supported image formats are JPEG and PNG.
+    The resulting tensor has dimensions NCHW (with N = 1).
+    When [resize] is set, the image is first resized preserving its original ratio
+    then a center crop is taken.
+*)
 val load_image
   :  ?resize:(int * int)
   -> string
   -> Tensor.t Or_error.t
 
+(** [load_images ?resize dir_name] is similar to applying [load_image] to all the images
+    in [dir_name].
+    The resulting tensor has dimensions NCHW where N is the number of images.
+*)
 val load_images : ?resize:(int * int) -> string -> Tensor.t
 
 val load_dataset
