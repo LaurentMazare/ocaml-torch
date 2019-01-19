b8fc481f1eaa9c3fb1ccc3fcbd43eba72912cd54
laurent <laurent.mazare@gmail.com>
2019-01-19 21:27:10 +0000

    Get DQN to work on CartPole-v1.

--- examples/reinforcement-learning/dqn.ml
+++ examples/reinforcement-learning/dqn.ml
@@ -35,6 +35,7 @@ module Replay_memory : sig
   val create : capacity:int -> _ t
   val push : 'a t -> 'a -> unit
   val sample : 'a t -> batch_size:int -> 'a list
+  val length : _ t -> int
 end = struct
   type 'a t =
     { memory : 'a Queue.t
@@ -47,6 +48,8 @@ end = struct
     ; capacity
     ; position = 0
     }
+
+  let length t = Queue.length t.memory
 
   let push t elem =
     if Queue.length t.memory < t.capacity
@@ -100,7 +103,8 @@ module DqnAgent : sig
   type t
   val create : state_dim:int -> actions:int -> memory_capacity:int -> t
   val action : t -> state -> int
-  val learn : t -> unit
+  val learn : t -> float option
+  val transition_feedback : t -> Transition.t -> unit
 end = struct
   type t =
     { model : Layer.t
@@ -141,21 +145,59 @@ end = struct
     end else Random.int t.actions
 
   let learn t =
-    let transitions = Replay_memory.sample t.memory ~batch_size:t.batch_size in
-    let states = Transition.batch_states transitions in
-    let next_states = Transition.batch_next_states transitions in
-    let actions = Transition.batch_actions transitions in
-    let rewards = Transition.batch_rewards transitions in
-    let qvalues =
-      Layer.apply t.model states
-      |> Tensor.gather ~dim:1 ~index:(Tensor.unsqueeze actions ~dim:1)
-      |> Tensor.squeeze1 ~dim:1
-    in
-    let next_qvalues, _ =
-      Layer.apply t.model next_states
-      |> Tensor.max2 ~dim:1 ~keepdim:false
-    in
-    let expected_qvalues = Tensor.(rewards + f t.gamma * next_qvalues) in
-    let loss = Tensor.mse_loss qvalues expected_qvalues in
-    Optimizer.backward_step t.optimizer ~loss
+    if t.batch_size <= Replay_memory.length t.memory
+    then begin
+      let transitions = Replay_memory.sample t.memory ~batch_size:t.batch_size in
+      let states = Transition.batch_states transitions in
+      let next_states = Transition.batch_next_states transitions in
+      let actions = Transition.batch_actions transitions in
+      let rewards = Transition.batch_rewards transitions in
+      let qvalues =
+        Layer.apply t.model states
+        |> Tensor.gather ~dim:1 ~index:(Tensor.unsqueeze actions ~dim:1)
+        |> Tensor.squeeze1 ~dim:1
+      in
+      let next_qvalues, _ =
+        Layer.apply t.model next_states
+        |> Tensor.max2 ~dim:1 ~keepdim:false
+      in
+      let expected_qvalues = Tensor.(rewards + f t.gamma * next_qvalues) in
+      let loss = Tensor.mse_loss qvalues expected_qvalues in
+      Optimizer.backward_step t.optimizer ~loss;
+      Some (Tensor.to_float0_exn loss)
+    end else None
+
+  let transition_feedback t transition = Replay_memory.push t.memory transition
 end
+
+(* Hard-code dimensions to CartPole-v1 for the time being. *)
+let gym_training () =
+  let module G = Openai_gym.Gym_client in
+  let state_of_observation obs =
+    Yojson.Basic.Util.to_list obs
+    |> List.map ~f:Yojson.Basic.Util.to_number
+    |> Tensor.float_vec
+  in
+  let instance_id = G.env_create "CartPole-v1" in
+  let agent = DqnAgent.create ~state_dim:4 ~actions:2 ~memory_capacity:5000 in
+  for episode_idx = 1 to 10000 do
+    let init = G.env_reset instance_id in
+    let rec loop state acc_reward =
+      let action = DqnAgent.action agent state in
+      let response = G.env_step instance_id { action } false in
+      let next_state = state_of_observation response.step_observation in
+      let reward = response.step_reward in
+      DqnAgent.transition_feedback agent { state; action; next_state; reward };
+      let _loss = DqnAgent.learn agent in
+      let acc_reward = reward +. acc_reward in
+      if response.step_done
+      then acc_reward
+      else loop next_state acc_reward
+    in
+    let reward = loop (state_of_observation init.observation) 0. in
+    Stdio.printf "%d %f\n%!" episode_idx reward;
+  done;
+  G.env_close instance_id
+
+let () =
+  gym_training ()
