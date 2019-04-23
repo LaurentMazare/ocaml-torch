ff8370e39cf19150fad957279247642ec27ab6aa
laurent <laurent.mazare@gmail.com>
2019-04-23 19:01:54 +0100

    Add a test for the newly added chunk function.

--- src/tests/tensor_tests.ml
+++ src/tests/tensor_tests.ml
@@ -8,7 +8,7 @@ let%expect_test _ =
   [%expect {|
         42
       |}];
-  let t = Tensor.float_vec [1.; 42.; 1337.] in
+  let t = Tensor.float_vec [ 1.; 42.; 1337. ] in
   Stdio.printf !"%{sexp:float array}\n" Tensor.(to_float1_exn (t * t));
   [%expect {|
         (1 1764 1787569)
@@ -20,11 +20,11 @@ let%expect_test _ =
 
 let%expect_test _ =
   let open Tensor in
-  let t = zeros [4; 2] in
-  t.%.{[1; 1]} <- 42.0;
-  t.%.{[3; 0]} <- 1.337;
+  let t = zeros [ 4; 2 ] in
+  t.%.{[ 1; 1 ]} <- 42.0;
+  t.%.{[ 3; 0 ]} <- 1.337;
   for i = 0 to 3 do
-    Stdio.printf "%f %f\n" (t.%.{[i; 0]}) (t.%.{[i; 1]})
+    Stdio.printf "%f %f\n" (t.%.{[ i; 0 ]}) (t.%.{[ i; 1 ]})
   done;
   [%expect
     {|
@@ -36,7 +36,7 @@ let%expect_test _ =
 
 let%expect_test _ =
   let open Tensor in
-  let t = zeros [5; 2] in
+  let t = zeros [ 5; 2 ] in
   t += f 1.;
   narrow t ~dim:0 ~start:1 ~length:3 += f 2.;
   narrow t ~dim:1 ~start:1 ~length:1 -= f 3.;
@@ -54,13 +54,13 @@ let%expect_test _ =
     (1 2 3) |}]
 
 let%expect_test _ =
-  let t = Tensor.of_int2 [|[|3; 4; 5|]; [|2; 3; 4|]|] in
+  let t = Tensor.of_int2 [| [| 3; 4; 5 |]; [| 2; 3; 4 |] |] in
   Tensor.(narrow t ~dim:1 ~start:0 ~length:1 += of_int0 42);
   Stdio.printf !"%{sexp:int array array}\n" (Tensor.to_int2_exn t);
   [%expect {| ((45 4 5) (44 3 4)) |}]
 
 let%expect_test _ =
-  let t = Tensor.zeros [2; 3; 2] in
+  let t = Tensor.zeros [ 2; 3; 2 ] in
   let u = Tensor.narrow t ~dim:1 ~start:1 ~length:2 in
   let v = Tensor.get u 1 in
   let w = Tensor.copy v in
@@ -74,7 +74,7 @@ let%expect_test _ =
   [%expect {| (((0 0) (0 0) (0 0)) ((0 0) (1 1) (1 1))) |}]
 
 let%expect_test _ =
-  let logits = Tensor.of_float1 [|-1.; 0.5; 0.25; 0.; 2.; 4.; -1.|] in
+  let logits = Tensor.of_float1 [| -1.; 0.5; 0.25; 0.; 2.; 4.; -1. |] in
   let eval_and_print ~target =
     let bce1 =
       Tensor.(bce_loss (sigmoid logits) ~targets:(ones_like logits * f target))
@@ -102,10 +102,20 @@ let%expect_test _ =
   [%expect {| 0.64496642351150513 0.64496642351150513 0.64496642351150513 |}]
 
 let%expect_test _ =
-  let vs = Tensor.of_float1 [|-1.01; -1.; -0.99; 0.5; 0.25; 0.; 2.; 4.; -1.; -3.|] in
+  let vs = Tensor.of_float1 [| -1.01; -1.; -0.99; 0.5; 0.25; 0.; 2.; 4.; -1.; -3. |] in
   Stdio.printf
     !"%{sexp:float array} %{sexp:float}\n"
     Tensor.(huber_loss vs (Tensor.f 0.) ~reduction:None |> to_float1_exn)
     Tensor.(huber_loss vs (Tensor.f 0.) |> to_float0_exn);
   [%expect
     {| (0.50999999046325684 0.5 0.49005001783370972 0.125 0.03125 0 1.5 3.5 0.5 2.5) 0.9656299352645874 |}]
+
+let%expect_test _ =
+  let vs = List.range 1 10 |> Array.of_list |> Tensor.of_int1 in
+  let chunk = Tensor.chunk vs ~chunks:4 ~dim:0 in
+  Stdio.printf
+    !"%{sexp:int array} %{sexp:int array list}\n"
+    (Tensor.to_int1_exn vs)
+    (List.map chunk ~f:Tensor.to_int1_exn);
+  [%expect
+    {| (1 2 3 4 5 6 7 8 9) ((1 2 3) (4 5 6) (7 8 9)) |}]
