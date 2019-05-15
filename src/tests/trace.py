778156a0115258fdeae5c46388a66819c6db89ef
laurent <laurent.mazare@gmail.com>
2019-05-15 20:25:44 +0100

    Adapt the JIT tests for PyTorch v1.1.

--- src/tests/trace.py
+++ src/tests/trace.py
@@ -1,21 +1,37 @@
 import torch
-def foo(x, y):
-  return 2 * x + y
+
+class Foo(torch.jit.ScriptModule):
+    def __init__(self):
+        super(Foo, self).__init__()
+
+    def forward(self, x, y):
+        return 2 * x + y
+
+foo = Foo()
 traced_foo = torch.jit.trace(foo, (torch.rand(3), torch.rand(3)))
-traced_foo.save("foo.pt")
+traced_foo.save('foo.pt')
 
-def foo2(x, y):
-  return (2 * x + y, x - y)
-traced_foo2 = torch.jit.trace(foo2, (torch.rand(3), torch.rand(3)))
-traced_foo2.save("foo2.pt")
+class Foo2(torch.jit.ScriptModule):
+    def __init__(self):
+        super(Foo2, self).__init__()
 
-def foo3_fn(x):
-  result = x[0]
-  for i in range(x.size(0)):
-    if i != 0:
-      result = result * x[i]
-  return result
+    def forward(self, x, y):
+        return (2 * x + y, x - y)
 
-foo3 = torch.jit.script(foo3_fn)
+foo = Foo2()
+traced_foo = torch.jit.trace(foo, (torch.rand(3), torch.rand(3)))
+traced_foo.save('foo2.pt')
 
-foo3.save("foo3.pt")
+class Foo3(torch.jit.ScriptModule):
+    def __init__(self):
+        super(Foo3, self).__init__()
+
+    @torch.jit.script_method
+    def forward(self, x):
+        result = x[0]
+        for i in range(x.size(0)):
+            if i: result = result * x[i]
+        return result
+
+foo = Foo3()
+foo.save('foo3.pt')
