548e0f1e5c99c53b020695369b87489627ba1c8d
Laurent Mazare <laurent.mazare@gmail.com>
2019-06-16 02:14:56 +0100

    Add a list of modules for the torch package.

---
+++ src/torch/torch.ml
@@ -0,0 +1,16 @@
+module Checkpointing = Checkpointing
+module Cifar_helper = Cifar_helper
+module Dataset_helper = Dataset_helper
+module Device = Device
+module Layer = Layer
+module Mnist_helper = Mnist_helper
+module Module = Module
+
+(* [Nn] is an alias to [Layer] to keep coherence with the pytorch names. *)
+module Nn = Layer
+module Optimizer = Optimizer
+module Scalar = Scalar
+module Serialize = Serialize
+module Tensor = Tensor
+module Text_helper = Text_helper
+module Var_store = Var_store
