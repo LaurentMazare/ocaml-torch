ffb24333cf59b11c28f28f9237616e065f285200
Laurent Mazare <laurent.mazare@gmail.com>
2019-07-07 22:02:10 +0100

    Multi-headed attention.

--- examples/transformer/transformer.ml
+++ examples/transformer/transformer.ml
@@ -34,6 +34,28 @@ let attention ~query ~key ~value ~mask ~dropout ~is_training =
     |> Tensor.softmax ~dim:(-1)
     |> Tensor.dropout ~p:dropout ~is_training
   in
-  Tensor.matmul p_attn value, p_attn
+  Tensor.matmul p_attn value
 
-let () = ignore (sublayer_connection, attention)
+let multi_headed_attention vs dim ~heads ~dropout =
+  let linear_q = Layer.linear vs ~input_dim:dim dim in
+  let linear_k = Layer.linear vs ~input_dim:dim dim in
+  let linear_v = Layer.linear vs ~input_dim:dim dim in
+  let linear_out = Layer.linear vs ~input_dim:dim dim in
+  let d_k = dim / heads in
+  fun ~query ~key ~value ~mask ~is_training ->
+    let mask = Tensor.unsqueeze mask ~dim:1 in
+    let batch_size = Tensor.size query |> List.hd_exn in
+    let view_transpose xs =
+      Tensor.view xs ~size:[ batch_size; -1; heads; d_k ]
+      |> Tensor.transpose ~dim0:1 ~dim1:2
+    in
+    let query = Layer.forward linear_q query |> view_transpose in
+    let key = Layer.forward linear_k key |> view_transpose in
+    let value = Layer.forward linear_v value |> view_transpose in
+    attention ~query ~key ~value ~mask ~dropout ~is_training
+    |> Tensor.transpose ~dim0:1 ~dim1:2
+    |> Tensor.contiguous
+    |> Tensor.view ~size:[ batch_size; -1; heads * d_k ]
+    |> Layer.forward linear_out
+
+let () = ignore (sublayer_connection, multi_headed_attention)
